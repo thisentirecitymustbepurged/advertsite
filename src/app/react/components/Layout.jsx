ee53e9314884b8ff4494d73eaa53dfac3704cc39
yogi <yogintas@gmail.com>
2017-07-06 18:27:28 +0300

    added user page

--- src/app/react/components/Layout.jsx
+++ src/app/react/components/Layout.jsx
@@ -4,7 +4,7 @@ import { Route } from 'react-router-dom'
 import Navigation from './containers/Navigation';
 import Home from './pages/Home';
 import About from './pages/About';
-import User from '.pages/User'
+import User from './pages/User'
 
 export default class Layout extends Component {
   render() {
