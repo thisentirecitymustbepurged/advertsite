e8263cc508ee14ba60ebb73d7c97e36e3208f78a
yogi <jogintassimutis@convendia.com>
2017-10-22 11:35:57 +0100

    working n it

---
+++ src/app/~core/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import AppBar from 'material-ui/AppBar';
+
+const Header = () => (
+  <AppBar
+
+  />
+);
+
+// const mapStateToProps = state => ({
+// });
+
+// const mapDispatchToProps = dispatch => bindActionCreators({
+// }, dispatch);
+
+export default Header;
+// export default connect(mapStateToProps, mapDispatchToProps)(Header);
